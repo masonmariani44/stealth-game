8ffbfb314589781d2d0b43ad38e6fb1f6e2ffff0
masonmariani44 <masonmariani44@gmail.com>
2026-02-13 22:15:09 -0700

    Initital Sidle Logic

--- Scripts/Player/States/walk_sidle_state.gd
+++ Scripts/Player/States/walk_sidle_state.gd
@@ -4,7 +4,12 @@ class_name WalkSidleState
 
 
 @export_range(0.0, 10.0) var SPEED := 5.0
-@export var wall_raycast : WallDetectionRay
+
+
+@export var wall_detect_raycast_forward  : WallDetectionRay
+@export var wall_leave_raycast_left      : WallDetectionRay
+@export var wall_leave_raycast_right     : WallDetectionRay
+@export var wall_leave_raycast_forward   : WallDetectionRay
 
 
 
@@ -13,11 +18,28 @@ class_name WalkSidleState
 
 
 func enter():
+	print("start sidle")
 	var character = state_machine.get_parent()
-	print("enter walk sidle state!")
-	if wall_raycast.cast() != null:
-		character.wall_inverse_normal = -wall_raycast.get_collision_normal()
-	print(character.wall_inverse_normal)
+	if wall_detect_raycast_forward.cast() != null:
+		character.wall_inverse_normal = -wall_detect_raycast_forward.get_collision_normal()
+
+
+	# https://kidscancode.org/godot_recipes/3.x/3d/3d_align_surface/
+	# good resource for rotation objects to surface normals. it's what i use here
+
+	# perform the rotation they specify then set the rotation's z to 0. the y value should automatically be set to 
+	# face the incoming direction, which means we don't neeed to check for an "angle_diff"
+
+	# rotate character to facing direction along wall
+
+	# TODO: Remove this?? unnecessary
+	#var angle_diff = character.wall_inverse_normal.signed_angle_to(character.transform.basis.z, Vector3.UP)
+
+	# step 2: if right, rotate character to face wall normal, then +(?) 90 degrees around up vector (-90 if left)
+	character.global_transform.basis.y = -character.wall_inverse_normal
+	character.global_transform.basis.x = -character.global_transform.basis.z.cross(-character.wall_inverse_normal)
+	character.global_transform.basis = character.global_transform.basis.orthonormalized()
+	character.rotation.z = 0
 
 	"""
 
@@ -35,6 +57,8 @@ func enter():
 
 func physics_update(delta):
 	var character = state_machine.get_parent()
+
+	character.remote_transform_visuals.update_rotation = false
 
 
 	"""
@@ -60,6 +84,10 @@ func physics_update(delta):
 	cleaning up the code a bit
 	
 
+
+
+
+
 	"""
 
 	# Add the gravity.
@@ -71,6 +99,11 @@ func physics_update(delta):
 	add force towards the wall. right now i'm not dynamically getting the closest wall just the OG
 	"""
 	
+
+
+
+
+
 		
 	# Get the input direction
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
@@ -89,32 +122,35 @@ func physics_update(delta):
 		# Rotate to face moving direction
 		var target_rotation := atan2(-direction.x, -direction.z)
 		#TODO: why cant I replace this 0.1 constant with a variable???? with the same value?? casuses spinning...
+		
 		character.rotation.y = lerp_angle(character.rotation.y, target_rotation, 0.1)
 
 
 
 
-		# !!!!!!!!! SIDLE CHANGES!!!!!!!!! 
+
+
+		# TODO: check if right or left raycast are still touching a wall
+		if character.is_on_wall():
+			if \
+			wall_leave_raycast_right.cast()   == null and \
+			wall_leave_raycast_left.cast()    == null and \
+			wall_leave_raycast_forward.cast() == null:
+				print("change back to walk state")
+				character.remote_transform_visuals.update_rotation = true
+				character.visuals.rotation = character.rotation
+				state_machine.change_state("WalkState")
 
 
 
-		# Check if sidle condition is met
-		# TODO: implement sidle states and transition here
-		#if character.is_on_wall():
-		#	if wall_raycast.cast() == null:
-		#		print("no")
-		#	else:
-		#		print("yes!!")
-
-
-
-		# !!!! end sidle !!!!!!
-		
 
 
 
 	else:
+		print("change back to idle sidle state")
 		state_machine.change_state("IdleSidleState")
+
+	
 
 	character.velocity += character.wall_inverse_normal * delta * character.WALL_PULL_SPEED
 	character.move_and_slide()
@@ -125,4 +161,5 @@ func physics_update(delta):
 
 func handle_input(_event):
 	if Input.is_action_just_pressed("jump"):
+		print("jump state!!")
 		state_machine.change_state("JumpState")
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 
 @onready var cam_pivot: Node3D = $CamPivot
+@onready var remote_transform_visuals: Node3D = $RemoteTransform3D_Visuals
+@onready var visuals: Node3D = $VisualsCollection/Visuals
 
 
 @export_range(0.0, 10.0)  var GRAVITY_SCALE := 1.0 	# NOTE: 2.8 feels nice
